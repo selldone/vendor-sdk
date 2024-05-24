f0de149788db5399b825dd5e4914978cb87b57a5
Mehrdad Pajuhaan <pajuhaan@gmail.com>
2024-05-24 20:29:05 +0400

    Update

--- product/requests/vapi.product.info.get.ts
+++ product/requests/vapi.product.info.get.ts
@@ -19,17 +19,17 @@ import type {ProductVariant} from "@selldone/core-js/models/shop/product/product
 import {Shop} from "@selldone/core-js/models/shop/shop.model";
 import {Vendor} from "@selldone/core-js/models/shop/vendor/vendor.model";
 import {Connect} from "@selldone/core-js/models/connect/connect.model";
-import type  {SubscriptionPrice} from "@selldone/core-js/models/shop/product/subscription_price.model";
+import type {SubscriptionPrice} from "@selldone/core-js/models/shop/product/subscription_price.model";
 import type {Valuation} from "@selldone/core-js/models/shop/accounting/valuation/valuation.model";
 import type {MapTag} from "@selldone/core-js/models/shop/map/map-tag.model";
 import type {ExtraPricing} from "@selldone/core-js/models/shop/extra-pricing/extra-pricing.model";
 import type {ShopInclude} from "@selldone/core-js/models/shop/shop-include/shop-include.model";
 import type {Page} from "@selldone/core-js/models/shop/page/page.model";
-import type{Article} from "@selldone/core-js/models/article/article.model";
-import type{ProductImage} from "@selldone/core-js/models/shop/product/product-image.model";
-import type{ProductRating} from "@selldone/core-js/models/shop/product/product-rating.model";
-import type{ProductFile} from "@selldone/core-js/models/shop/product/product-file.model";
-import type{ProductData} from "@selldone/core-js/models/shop/product/product-data.model";
+import type {Article} from "@selldone/core-js";
+import type {ProductImage} from "@selldone/core-js/models/shop/product/product-image.model";
+import type {ProductRating} from "@selldone/core-js/models/shop/product/product-rating.model";
+import type {ProductFile} from "@selldone/core-js/models/shop/product/product-file.model";
+import type {ProductData} from "@selldone/core-js/models/shop/product/product-data.model";
 import {DeliveryStateCode} from "@selldone/core-js/enums/delivery/DeliveryStateCode";
 import {OrderTypeCode} from "@selldone/core-js/enums/order/OrderTypeCode";
 
@@ -41,10 +41,13 @@ export default function vapiProductInfoGet(
   limit: number,
   options?: vapi.products.info.get.IParams,
 ) {
-  const params = {offset: offset, limit: limit, ...options}
-  const url = window.VAPI.GET_MY_VENDOR_PRODUCT_INFO_ADMIN(vendor_id,product_id);
+  const params = { offset: offset, limit: limit, ...options };
+  const url = window.VAPI.GET_MY_VENDOR_PRODUCT_INFO_ADMIN(
+    vendor_id,
+    product_id,
+  );
 
-  return this.getNow<vapi.products.info.get.IResponse>(url,params)
+  return this.getNow<vapi.products.info.get.IResponse>(url, params);
 }
 
 //█████████████████████████████████████████████████████████████
@@ -52,34 +55,99 @@ export default function vapiProductInfoGet(
 //█████████████████████████████████████████████████████████████
 
 export namespace vapi.products.info.get {
-
   export interface IResponse {
     product: Product & {
-      product_variants: ProductVariant[], shop: Pick<Shop, 'id' | 'name'>, parent: Product & { shop: Pick<Shop, 'id' | 'title' | 'name' | 'description' | 'icon'> } | null/*Dropshipping main product*/
-      , vendor?: Pick<Vendor, 'id' | 'name' | 'description' | 'icon' | 'enable' | 'status' | 'email' | 'address' | 'web' | 'tel' | 'payment_at'> | null // Marketplace > Add vendor info
-      , connect: Pick<Connect, 'id' | 'code' | 'name' | 'description' | 'icon' | 'enable'> | null  //🦠 Connect info
-      , subscription_prices: SubscriptionPrice[]      // 🎗️ Subscription
-      , valuation: Valuation           // Valuation (Custom pricing)
-      , map: MapTag
+      product_variants: ProductVariant[];
+      shop: Pick<Shop, "id" | "name">;
+      parent:
+        | (Product & {
+            shop: Pick<Shop, "id" | "title" | "name" | "description" | "icon">;
+          })
+        | null /*Dropshipping main product*/;
+      vendor?: Pick<
+        Vendor,
+        | "id"
+        | "name"
+        | "description"
+        | "icon"
+        | "enable"
+        | "status"
+        | "email"
+        | "address"
+        | "web"
+        | "tel"
+        | "payment_at"
+      > | null; // Marketplace > Add vendor info
+      connect: Pick<
+        Connect,
+        "id" | "code" | "name" | "description" | "icon" | "enable"
+      > | null; //🦠 Connect info
+      subscription_prices: SubscriptionPrice[]; // 🎗️ Subscription
+      valuation: Valuation; // Valuation (Custom pricing)
+      map: MapTag;
       // 🌸 Extra pricing 🌸 [Vary pricing by quantity]
-      , extra_pricings: ExtraPricing[], includes: ShopInclude[], page: Pick<Page, 'id' | 'user_id' | 'published' | 'title' | 'image' | 'name' | 'color'> | null
-      ,category:Pick<Category,'id'| 'title'| 'name'| 'description'| 'parent_id'| 'icon'> & {parent:Pick<Category,'id'| 'title'| 'name'| 'description'| 'parent_id'| 'icon'> | null}
-      ,images:ProductImage[]
-      ,ratings:Pick<ProductRating, 'id'| 'name'| 'value'| 'count'>[]
-      ,files?:(Pick<ProductFile, 'id'|'product_id'|'sample'|'name'|'size'|'uploading'|'downloads'|'download_at'|'created_at'>)[] // Only load files for file products! Subscription may have thousands files for contents.
-      ,articles:(Article &{can_edit:boolean,tags:string[]})[]
+      extra_pricings: ExtraPricing[];
+      includes: ShopInclude[];
+      page: Pick<
+        Page,
+        "id" | "user_id" | "published" | "title" | "image" | "name" | "color"
+      > | null;
+      category: Pick<
+        Category,
+        "id" | "title" | "name" | "description" | "parent_id" | "icon"
+      > & {
+        parent: Pick<
+          Category,
+          "id" | "title" | "name" | "description" | "parent_id" | "icon"
+        > | null;
+      };
+      images: ProductImage[];
+      ratings: Pick<ProductRating, "id" | "name" | "value" | "count">[];
+      files?: Pick<
+        ProductFile,
+        | "id"
+        | "product_id"
+        | "sample"
+        | "name"
+        | "size"
+        | "uploading"
+        | "downloads"
+        | "download_at"
+        | "created_at"
+      >[]; // Only load files for file products! Subscription may have thousands files for contents.
+      articles: (Article & { can_edit: boolean; tags: string[] })[];
 
       //  🎗️ Only Subscription
-      ,linked_products:(Pick<Product,'id'| 'shop_id'| 'title'| 'icon'| 'sku'| 'mpn'| 'type'> & {product_variants:Pick<ProductVariant, 'id'| 'product_id'| 'image'| 'color'| 'style'| 'volume'| 'weight'| 'pack'| 'type'| 'sku'| 'mpn'>[]})[]
-      ,subscribers_count:number
+      linked_products: (Pick<
+        Product,
+        "id" | "shop_id" | "title" | "icon" | "sku" | "mpn" | "type"
+      > & {
+        product_variants: Pick<
+          ProductVariant,
+          | "id"
+          | "product_id"
+          | "image"
+          | "color"
+          | "style"
+          | "volume"
+          | "weight"
+          | "pack"
+          | "type"
+          | "sku"
+          | "mpn"
+        >[];
+      })[];
+      subscribers_count: number;
     };
     product_data: ProductData[];
-    orderQue: {delivery_state:DeliveryStateCode,count:number,type:OrderTypeCode}[];
+    orderQue: {
+      delivery_state: DeliveryStateCode;
+      count: number;
+      type: OrderTypeCode;
+    }[];
   }
 
   export interface IParams {
-
     with_trashed: boolean;
-
   }
 }
